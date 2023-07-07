822464367568780bbbcc9efd65b3b8418e80823c
VictorOrrillo <victor.orillo@tecsup.edu.pe>
2023-07-07 09:39:19 -0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/tecsup/facialrecognition/facial_recognition/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tecsup.facialrecognition.facial_recognition
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/page/login_page.dart
@@ -0,0 +1,74 @@
+import 'package:facial_recognition/page/face_recognition/camera_page.dart';
+import 'package:facial_recognition/utils/local_db.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  void initState() {
+    print(LocalDB.getUser().name);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+    appBar: AppBar(
+      title: const Text("Face Authentication"),
+      centerTitle: true,
+    ),
+    body: Padding(
+      padding: const EdgeInsets.all(32),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            buildButton(
+              text: 'Register',
+              icon: Icons.app_registration_rounded,
+              onClicked: () async {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const FaceScanScreen()));
+              },
+            ),
+            const SizedBox(height: 24),
+            buildButton(
+              text: 'Login',
+              icon: Icon.Login,
+              onClicked: () async {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => FaceScanScreen(user: LocalDB.getUser()));
+              },
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+
+  Widget buildButton({
+    required String text,
+    required IconData icon,
+    required VoidCallback onCLicked,
+  }) =>
+      ElevatedButton.icon(
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size.fromHeight(50),
+        ),
+        icon: Icon(icon, size: 26),
+        label: Text(
+          text,
+          style: const TextStyle(fontSize :20),
+        ),
+        onPressed: onCLicked,
+      );
+}
